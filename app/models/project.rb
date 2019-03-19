5216fa07df5be9c4fa60036aec66e7320cb8fb6b
Muhammad Moawaz Ayub <moawazayubmmm@gmail.com>
2019-03-19 17:30:28 +0500

    inital bug table created

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
+	has_many :bugs
 	has_many :assignments
 	has_many :users, through: :assignments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_19_094200) do
+ActiveRecord::Schema.define(version: 2019_03_19_114702) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer "user_id"
@@ -19,6 +19,18 @@ ActiveRecord::Schema.define(version: 2019_03_19_094200) do
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_assignments_on_project_id"
     t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
+
+  create_table "bugs", force: :cascade do |t|
+    t.string "title"
+    t.date "deadline"
+    t.string "screenshot"
+    t.string "type"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id"
+    t.index ["project_id"], name: "index_bugs_on_project_id"
   end
 
   create_table "projects", force: :cascade do |t|
