8272bb6daf1e96f067bc5e4e2a7c2601f551065f
Muhammad Moawaz Ayub <moawazayubmmm@gmail.com>
2019-03-21 14:26:01 +0500

    Bug routes added

---
+++ app/controllers/bugs_controller.rb
@@ -0,0 +1,19 @@
+class BugsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  get 'bugs/index'
+  get 'bugs/create'
+  get 'bugs/edit'
+  get 'bugs/new'
+  get 'bugs/show'
+  get 'bugs/update'
   #get 'projects/index'
   #get 'projects/new'
   #get 'projects/edit'
   #get 'projects/show'
   #get 'projects/create'
-  resources :projects
+  resources :projects do
+  	resources :bugs
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'dashboard#index'
