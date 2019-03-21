834e03283f30639ccdf07f2bbbf8e4d2a8ab0a6d
Muhammad Moawaz Ayub <moawazayubmmm@gmail.com>
2019-03-21 10:47:00 +0500

    manager can edit and delete projects

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,51 @@
+class ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :edit, :update, :destroy]
+
+  def destroy
+    @project.destroy
+    redirect_to @project
+  end
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def edit
+  end
+
+  def show
+    @bugs = @project.bugs
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def find_project
+    @project = Project.find(params[:id])
+  end
+
+    def project_params
+    params.require(:project).permit(:title, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #get 'projects/index'
+  #get 'projects/new'
+  #get 'projects/edit'
+  #get 'projects/show'
+  #get 'projects/create'
+  resources :projects
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'dashboard#index'
